10a3acfef2f8506939d921b8b76d24f7949f9404
ShadewRG <onankman@gmail.com>
2020-11-18 22:01:23 +0100

    Fix method descriptors not being methods

--- src/main/java/net/shadew/asm/descriptor/MethodDescriptor.java
+++ src/main/java/net/shadew/asm/descriptor/MethodDescriptor.java
@@ -18,6 +18,11 @@ public final class MethodDescriptor extends Descriptor {
         this.returnType = returnType;
     }
 
+    @Override
+    public boolean isMethod() {
+        return true;
+    }
+
     public TypeDescriptor parameter(int index) {
         return parameters[index];
     }
